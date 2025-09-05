ee3954ff5f4ec1a65337edc459497ad3a1e17cbd
Brian <brianpasco1206@gmail.com>
2025-09-05 12:53:24 +0800

    feat(global): connect project to mysql database

--- paper_trail_ecommerce_project/settings.py
+++ paper_trail_ecommerce_project/settings.py
@@ -1,3 +1,7 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 """
 Django settings for paper_trail_ecommerce_project project.
 
@@ -73,9 +77,13 @@ WSGI_APPLICATION = 'paper_trail_ecommerce_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
