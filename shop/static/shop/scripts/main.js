aa988bb4f1a9066a51e7feb64c75213b97370c09
Brian <brianpasco1206@gmail.com>
2025-09-23 17:00:01 +0800

    refactor(static & templates): improve javascript integration

---
+++ shop/static/shop/scripts/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    
+    console.log("Initializing main JavaScript entry file!");
+    console.log("Main JavaScript entry file successfully initialized!");
+
+});
