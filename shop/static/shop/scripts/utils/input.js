4e9012eccf43053dad4e9187ca8921f7c5a7f8b6
Brian <brianpasco1206@gmail.com>
2025-09-29 19:07:23 +0800

    feat(static/scripts): add handleFormInputsChecking function in input utilities

--- shop/static/shop/scripts/utils/input.js
+++ shop/static/shop/scripts/utils/input.js
@@ -6,3 +6,20 @@ export const handlePasswordToggle = (input, callback) => {
 
 };
 
+export const handleFormInputsChecking = (formInputs, callback) => {
+
+    if (formInputs.length <= 0) return;
+
+    for (const formInput of formInputs) {
+
+        if (!formInput.value || formInput.value.trim() === '') {
+            callback(false);
+            return;
+        }
+
+    }
+
+    callback(true);
+    
+};
+
